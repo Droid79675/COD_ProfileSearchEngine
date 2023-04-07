6000480ece308dbe575ad62144e6e1345830e065
Roman <roman9900@inbox.ru>
2023-04-07 18:57:15 +0300

    Second update

---
+++ CODSearchEngineProfile/app/src/main/java/com/codsearchengineprofile/presentation/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.codsearchengineprofile.presentation
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.codsearchengineprofile.databinding.ActivityMainBinding
+import com.codsearchengineprofile.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+}
