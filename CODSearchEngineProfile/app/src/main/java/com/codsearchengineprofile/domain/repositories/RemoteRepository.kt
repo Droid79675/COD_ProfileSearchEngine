6e7a5a8267c76ff962629a09adbfb4393aed6085
Roman <roman9900@inbox.ru>
2023-04-19 10:28:11 +0300

    Removing copies after merging commit from Data_and_domain_layers branch to master

--- CODSearchEngineProfile/app/src/main/java/com/codsearchengineprofile/domain/repositories/RemoteRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.codsearchengineprofile.domain.repositories
-
-import com.codsearchengineprofile.domain.models.ProfileDomainModel
-import io.reactivex.Single
-
-interface RemoteRepository {
-    fun getResponseProfiles(): Single<ProfileDomainModel>
-}
